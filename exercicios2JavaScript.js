2ba293790adf8118b2a113cc0c55084ee57a893f
Cecilia Sousa <ceciliasousa21@gmail.com>
2019-09-19 16:58:40 -0300

    resolve primeiros exercicios do arquivo 2

--- exercicios2JavaScript.js
+++ exercicios2JavaScript.js
@@ -1,26 +1,50 @@
-// Escreva a expressão em que atribui o
+//1. Escreva a expressão em que atribui o
 // valor `1` a `x` se `x` maior do que `y`.
 
-if (x > y); x = 1
-
-function atribuiValor(x, y){
-    if (x > y){
-        return x = 1
+function atribuiValor(a, b){
+    if (a > b){
+       return x = 1
     }
 }
 
-//Agora reescreva a expressão anterior
+// ou
+
+let x = 3
+let y = 2
+
+console.log("Valor de x antes do if",x)
+if (x > y){
+    x = 1
+    console.log("Valor de x depois do if",x)
+}
+
+console.log("Valor de x após sair do if",x)
+
+//2. Agora reescreva a expressão anterior
 //atribuindo 0 a `x` caso ele seja menor ou igual a y
 
-if (x <= y); x = 0
+x > y ? x = 1 : x = 0
 
-function atribuiValor(x, y){
-    if (x <= y){
+function atribuiValor(a, b){
+    if (a > b){
+        return x = 1
+    } else {
         return x = 0
     }
 }
 
-// Você foi contratada para desenvolver um jogo,
+// ou
+
+let x = 3
+let y = 2
+
+if (x > y) {
+    x = 1
+} else {
+    x = 0
+}
+
+//3. Você foi contratada para desenvolver um jogo,
 // a cada vez que o usuário acerta, ele ganha 5 pontos.
 // Quando se atinge a pontuação 100 ele entra na faixa
 // bônus e sua pontuação é duplicada até ele atingir 
@@ -46,23 +70,23 @@ function jogar(resultado){
 }
 
 
-// Reescreva a seguinte condicional
+//4. Reescreva a seguinte condicional
 // sem usar o operador `!`:
 // item = ! ( i < 10 || v >= 50 )
 
-item == (i >= 10 || v < 50)
+item = i >= 10 && v < 50
 
 // refs: 
 // - http://www.math.toronto.edu/preparing-for-calculus/3_logic/we_3_negation.html
 // - https://centraldefavoritos.com.br/2017/01/02/negacao-de-proposicoes/ (ver: "Negação de uma Proposição Disjuntiva")
 
 
-// Escreva um código que receba um valor 
+//5. Escreva um código que receba um valor 
 // digitado pelo usuário e mostre `par`
 // se o número for par e `impar`
 // se for impar.
 
-function numeroEhPar(){
+function parOuImpar(){
     if (prompt("Digite um número!")%2 == 0){
         return "Esse número é par!"
     } else {
@@ -70,14 +94,22 @@ function numeroEhPar(){
     }
 }
 
-// Escreva a expressão que retorna `positivo` 
+//6. Escreva a expressão que retorna `positivo` 
 // se dois números forem positivos
 // e `negativo` se dois números 
 // forem negativos.
 
+function positivoOuNegativo(num1, num2){
+    if (num1 >= 0 && num2 >= 0){
+        return "Positivo"
+    } else if (num1 < 0 && num2 < 0 ) {
+        return "Negativo"
+    } else {
+        return "Os sinais são diferentes!"
+    }
+}
 
-
-// Escreva a expressão em que retorna `true` se
+//7. Escreva a expressão em que retorna `true` se
 // dois números tiverem os mesmos
 // sinais (+ / -) e `false` 
 // se tiverem sinais
@@ -86,15 +118,30 @@ function numeroEhPar(){
 if (num1 >=0 && num2 >= 00 || num1 < 0 && num2 < 0); return true
 
 function comparaSinais(num1, num2){
-    if (num1 >=0 && num2 >= 00 || num1 < 0 && num2 < 0) {
+    if (num1 >=0 && num2 >= 0 || num1 < 0 && num2 < 0) {
         return true
     } else {
         return false
     }
 }
 
+// ou
 
-// Duas condicionais são equivalentes
+let x = 1, y = -2
+let sinalx = Math.sign(x)
+let sinaly = Math.sign(y)
+
+if (sinalx > 0 && sinaly > 0 || sinalx < 0 && sinaly < 0){
+    return true
+} else {
+    return false
+}
+
+// ou
+
+Math.sign(sinalx*sinaly)>0?true:false
+
+//8. Duas condicionais são equivalentes
 // quando, dado um mesmo input, devolvem
 // o mesmo output. Qual das próximas
 // condicionais são equivalentes? Porque?
@@ -134,7 +181,9 @@ if (rand === 0) {
 	console.log(`O valor é zero!`)
 }
 
-// Reescreva a próxima expressão usando
+// Resposta: A e C!
+
+//9. Reescreva a próxima expressão usando
 // um if / else.
 // switch (day) {
 // 	case 0: 
@@ -162,8 +211,27 @@ if (rand === 0) {
 // 		throw "Dia inválido!"
 // }
 
+function diaDaSemana(day){
+    if (day == 0){
+        console.log("Segunda")
+    } else if (day == 1) {
+        console.log("Terça")
+    } else if (day == 2){
+        console.log("Quarta")
+    } else if (day == 3) {
+        console.log("Quinta")
+    } else if (day == 4){
+        console.log("Sexta")
+    } else if (day == 5){
+        console.log("Sábado")
+    } else if (day == 6){
+        console.log("Domingo")
+    } else {
+        throw "Dia inválido!"
+    }
+}
 
-// Reescreva a próxima expressão usando
+//10. Reescreva a próxima expressão usando
 // o operador ternário
 // let rand
 // if (Math.random() > 0.5) {
@@ -172,8 +240,9 @@ if (rand === 0) {
 // 	rand = false
 // }
 
+let rand = (Math.random() > 0.5)?true:false
 
-// Reescreva a próxima expressão usando
+//11. Reescreva a próxima expressão usando
 // o operador ternário
 // let color
 // if (r > g && r > b) {
@@ -184,8 +253,14 @@ if (rand === 0) {
 // 	color = "Azul"
 // }
 
+let color = (r > g && r > b)?"Vermelho":(g > b && g > r)?"Verde":(b > r && b > g)?"Azul":"Cinza"
 
-// Lembra do exercício de par ou ímpar, vamos incrementar?
+// ou
+
+let color = (r > g && r > b)?"Vermelho":(g > b && g > r)?"Verde":"Azul"
+
+
+//12. Lembra do exercício de par ou ímpar, vamos incrementar?
 // Crie um jogo de par ou ímpar contra o compudador. 
 // A ideia é você gerar um número aleatório para o 
 // computador e comparar com o número informado pelo usuário e 
@@ -195,9 +270,36 @@ if (rand === 0) {
 //  - Use o prompt() para coletar o número informado pelo usuário
 
 
-// Construa a logica do menu de clientes de um site
+let opcaoUsuario = "par"
+let numUsuario = 1
+let numComputador = Math.floor(Math.random()*2)
+console.log(numComputador)
+let resultadoPar = (numUsuario + numComputador)%2 == 0
+
+if (opcaoUsuario == "par" && resultadoPar == true){
+    console.log("Você ganhou!")
+} else if (opcaoUsuario == "ímpar" && resultadoPar == false) {
+    console.log("Você ganhou!")
+} else {
+    console.log("Você perdeu!")
+}
+
+// ou (a terminar ainda)
+
+function jogoParOuImpar(){
+    let apostaUsuario = prompt("Digite um número de 0 a 9")
+    let apostaComputador = console.log(Math.floor(Math.random()*10))
+    if ((apostaUsuario + apostaComputador)%2 == 0 && apostaUsuario%2 == 0 || (apostaUsuario + apostaComputador)%2 != 0 && apostaUsuario%2 != 0){
+        return "Você ganhou!"
+    } else {
+        return "Você perdeu!"
+    }
+}
+
+//13. Construa a logica do menu de clientes de um site
 // caso o cliente seja perfil clássico, apresente uma ul
 // contendo nos li os itens terno, blazer, camisa,calça. Caso
 // o cliente seja do perfil esportivo mostre os itens: camisetas,
 // short, bermudas, moleton. Caso o cliente não informe o perfil, 
-//mostre todas as opções
+// mostre todas as opções
+
